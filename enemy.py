6daf10b243bf680fb9808661d29f8157ef15f65c
cc1005 <conorkampbell@hotmail.com>
2021-04-15 13:49:57 +0100

    Added logic to allow the enemy to fall down the screen. Simple if/else loop, adding on to the y axis if the enemy is not past the bottom of the screen, in which case its position gets reset to the top of the screen.

--- enemy.py
+++ enemy.py
@@ -3,16 +3,18 @@ import random
 import sys
 
 enemy_size = 30
-game_screen_width_for_enemy = 0
-enemy_pos = [0, 0]
 enemy_colour = (150, 252, 75) #Going for a lime green here
+enemy_speed = 10
 
 class EnemyClass:
     def SetScreenWidthForEnemy(game_screen_width):
-        game_screen_width_for_enemy = game_screen_width
         global enemy_pos
-        enemy_pos = [random.randint(0, game_screen_width_for_enemy), 0]
+        enemy_pos = [random.randint(0, game_screen_width - enemy_size), 0]
     
-    def EnemyRender(game_screen):
+    def EnemyRender(game_screen, game_screen_height):
+        if enemy_pos[1] >= 0 and enemy_pos[1] < game_screen_height:
+            enemy_pos[1] += enemy_speed
+        else:
+            enemy_pos[1] = 0
         pygame.draw.rect(game_screen, enemy_colour, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
 
--- game.py
+++ game.py
@@ -11,10 +11,13 @@ player_colour = (200, 200, 252)
 player_size = 30
 player_pos = [(screen_width/2), screen_height/2 + (screen_height/5 * 2)]
 background_colour = [100, 100, 100]
+movement_speed = 15
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 game_over = False
+
+clock = pygame.time.Clock()
 
 EnemyClass.SetScreenWidthForEnemy(screen_width)
 
@@ -28,7 +31,6 @@ while not game_over:
     if event.type == pygame.KEYDOWN:
         x = player_pos[0]
         y = player_pos[1]
-        movement_speed = 0.5
         if event.key == pygame.K_LEFT:
             x -= movement_speed
         elif event.key == pygame.K_RIGHT:
@@ -39,5 +41,8 @@ while not game_over:
     screen.fill((background_colour))
 
     pygame.draw.circle(screen, player_colour, (player_pos[0], player_pos[1]), player_size)
-    EnemyClass.EnemyRender(screen)
+    EnemyClass.EnemyRender(screen, screen_height)
+
+    clock.tick(30)
+
     pygame.display.update()
