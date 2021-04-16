30e18de881a92d7accbc761ee389a3cfebf52c5e
cc1005 <conorkampbell@hotmail.com>
2021-04-16 22:47:48 +0100

    Created a new file to manage collisions - logic in process.

---
+++ collision.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+from enemy import EnemyClass
+from player import PlayerClass
+
+def detect_collision(player_pos, enemy_pos)
+    p_x = player_pos[0]
+    p_y = player_pos[1]
+
+    e_x = enemy_pos[0]
+    e_y = enemy_pos[y]
--- player.py
+++ player.py
@@ -7,10 +7,13 @@ movement_speed = 15
 player_pos = [0, 0]
 
 class PlayerClass:
-
+    
     def PlayerFirstPosition(screen_height, screen_width):
         global player_pos
         player_pos = [(screen_width/2), screen_height/2 + (screen_height/5 * 2)]
+
+    def ReturnPlayerPos():
+        return player_pos
 
     def PlayerRender(screen):
         pygame.draw.rect(screen, player_colour, (player_pos[0], player_pos[1], player_size, player_size))
